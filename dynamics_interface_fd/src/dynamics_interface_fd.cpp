e6023bd2562d2e26afb20929ece19cd980f66703
Thibault Poignonec <t.poignonec@sherpa-mr.com>
2025-01-18 19:26:51 +0100

    lint

--- dynamics_interface_fd/src/dynamics_interface_fd.cpp
+++ dynamics_interface_fd/src/dynamics_interface_fd.cpp
@@ -349,7 +349,8 @@ bool DynamicsInterfaceFd::calculate_inertia(
   if (fd_inertia_msg_->data.size() != static_cast<size_t>(36))
   {
     RCLCPP_ERROR(
-      LOGGER, "The size of the inertia matrix msg (%zu) does not match the required size of (6 x 6)",
+      LOGGER,
+      "The size of the inertia matrix msg (%zu) does not match the required size of (6 x 6)",
       fd_inertia_msg_->data.size());
     return false;
   }
